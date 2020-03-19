77a27b1dedecf21b9f2cdc2f5674afeae392c6be
Srikrishna <srike27@gmail.com>
2020-03-20 01:15:37 +0530

    added growth cap speed and stamina reduction

--- animal.py
+++ animal.py
@@ -8,10 +8,11 @@ class animal:
     def __init__(self,x,y):
         self.size = np.random.uniform(low = 5.0,high = 50)
         self.size_norm = self.size/50.0
+        self.size_cap = 50
         self.speed = np.random.uniform(low = 0.0, high = 10.0)
         self.speed_norm = self.speed/10.0
         self.attack = np.random.uniform(low =0.0,high = 1.0)
-        self.stamina = np.random.uniform(low = 0.0, high = 1.0)
+        self.stamina_cap = np.random.uniform(low = 0.0, high = 1.0)
         self.aggressiveness = np.random.uniform(low = 0.0, high = 1.0)
         self.metabolism = np.random.uniform(low = 0.0, high = 1.0)
         self.sight = np.random.uniform(low = 0.0, high = 200.0)
@@ -21,16 +22,17 @@ class animal:
         self.brate = np.random.uniform(low = 0.0, high = 1.0)
         self.drate = np.random.uniform(low = 0.0, high = 1.0)
         self.normalize()
+        self.stamina = 100.0
         self.px = x
         self.py = y
         self.velx = 0
         self.vely = 0
         self.ax = 0
         self.ay = 0
-        self.speed =math.sqrt(self.velx*self.velx+self.vely*self.vely)
         self.vthresh = self.speed
 
     def updatestate(self):
+        self.erate = self.size/200
         tmvx = self.velx
         tmvy = self.vely
         self.px+=self.velx
@@ -38,6 +40,11 @@ class animal:
         self.velx = self.velx + self.ax
         self.vely = self.vely + self.ay
         self.cspd =math.sqrt(self.velx*self.velx+self.vely*self.vely)
+        print(self.speed)
+        if self.cspd > self.speed:
+            self.velx = self.velx/self.cspd*self.speed
+            self.vely = self.vely/self.cspd*self.speed
+        self.stamina -= (self.metabolism)*(self.cspd)
         if self.ax==0:
             self.velx = tmvx
         if self.ay==0:
@@ -51,14 +58,14 @@ class animal:
         self.ay = 0
 
     def grow(self):
-        self.size *= 1 + (1-self.metabolism)*(self.grate)/100     
-
+        if self.size <= self.size_cap:
+            self.size *= 1 + (1-self.metabolism)*(self.grate)/100
 
     def normalize(self):
-        total = np.sqrt(self.grate**2 + self.aggressiveness**2+self.attack**2+self.brate**2+self.drate**2+self.hunger**2+self.sight_norm**2+self.size_norm**2+self.stamina**2+self.metabolism**2+self.speed_norm**2)
+        total = np.sqrt(self.grate**2 + self.aggressiveness**2+self.attack**2+self.brate**2+self.drate**2+self.hunger**2+self.sight_norm**2+self.size_norm**2+self.stamina_cap**2+self.metabolism**2+self.speed_norm**2)
         self.size /= total
         self.speed /= total
-        self.stamina /= total
+        self.stamina_cap /= total
         self.attack /= total
         self.aggressiveness /= total
         self.metabolism /= total
