cfb2b37027b4eddec55693505a119b0aaa3d6027
Srikrishna <srike27@gmail.com>
2020-03-25 07:02:30 +0530

    changed multiplier for better distribution

--- roulette.py
+++ roulette.py
@@ -22,7 +22,7 @@ class roulette:
         self.reproduce_vect = a.brate*a.drate*(1-a.grate)*(1-a.hunger)
         self.fight_vect = a.aggressiveness*a.attack*a.stamina*a.size_norm
         self.flight_vect = a.speed_norm*a.stamina*a.drate
-        self.feed_vect = a.hunger*(1-a.stamina)*a.brate*a.grate
+        self.feed_vect = 3*a.hunger*(1-a.stamina)*a.brate*a.grate
         total = self.reproduce_vect + self.feed_vect + self.flight_vect + self.fight_vect
         self.reproduce_p = self.reproduce_vect/total
         self.fight_p = self.fight_vect/total
